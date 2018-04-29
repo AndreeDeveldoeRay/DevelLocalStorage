8e002c198d1d2169c486cac59f861bc9d10f1b71
develdoe <andreeray@live.com>
2018-04-29 17:01:48 +0200

    es2015

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-/**
- * @Author: Andreee Ray <develdoe>
- * @Date:   2017-04-11T14:26:37+02:00
- * @Email:  me@andreeray.se
- * @Filename: index.js
- * @Last modified by:   develdoe
- * @Last modified time: 2017-04-19T12:54:52+02:00
- */
+exports.DevelLocalStorage = {
+    /**
+     * Takes an array and transforms it to a string for localStorage
+     * and stores is as 'store'
+     * @param {Array} data
+     */
+    set: function(store, data) {
+        if (Array.isArray(data)) {
+            localStorage.setItem(store, JSON.stringify(data))
+            return data
+        }
+    },
+    /**
+     * Gets the 'store in localStorage and parses it back to array (JSON)
+     * @return {Array} returns the store as an array if it exist otherwise
+     * returns an empy array
+     */
+    get: function(store) {
+        var stringArray = localStorage.getItem(store)
+        var array = []
+        try {
+            array = JSON.parse(stringArray)
+        } catch (e) {
+            console.log(e)
+        }
+        return Array.isArray(array) ? array : []
+    }
 
-
-let DevelLocalStorage = {
-     /**
-      * Takes an array and transforms it to a string for localStorage
-      * and stores is as 'store'
-      * @param {Array} data
-      */
-     set: function(store, data) {
-         if (Array.isArray(data)) {
-             localStorage.setItem(store, JSON.stringify(data))
-             return data
-         }
-     },
-     /**
-      * Gets the 'store in localStorage and parses it back to array (JSON)
-      * @return {Array} returns the store as an array if it exist otherwise
-      * returns an empy array
-      */
-     get: function(store) {
-         var stringArray = localStorage.getItem(store)
-         var array = []
-         try {
-             array = JSON.parse(stringArray)
-         } catch (e) {
-             console.log(e)
-         }
-         return Array.isArray(array) ? array : []
-     }
-
- }
-
- export default DevelLocalStorage
+}
